a9cc08387d3f4eb1fef7738588192e6b53871b85
Thord Möller <thord_moller_92@hotmail.com>
2024-02-21 09:25:18 +0100

    final touches

--- QuotationService/Data/DataContext.cs
+++ QuotationService/Data/DataContext.cs
@@ -8,5 +8,21 @@ namespace QuotationService.Data
 
         public DbSet<City> Cities { get; set; }
         public DbSet<Service> Services { get; set; }
+
+        //lägger in data som behövs i databasen
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<City>().HasData(
+                new City { Id = 1, Name = "Stockholm", Rate = 65 },
+                new City { Id = 2, Name = "Uppsala", Rate = 55 }
+            );
+            modelBuilder.Entity<Service>().HasData(
+                new Service { Id = 1, Name = "Fönsterputs", Price = 300, CityId = 1 },
+                new Service { Id = 2, Name = "Balkongstädning", Price = 150, CityId = 1 },
+
+                new Service { Id = 3, Name = "Fönsterputs", Price = 300, CityId = 2 },
+                new Service { Id = 4, Name = "Balkongstädning", Price = 150, CityId = 2 },
+                new Service { Id = 5, Name = "Bortforsling av skräp", Price = 400, CityId = 2 }
+                );
+        }
     }
 }
---
+++ QuotationService/Migrations/20240221080656_databasepopulation.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuotationService.Migrations
+{
+    public partial class databasepopulation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Name", "Rate" },
+                values: new object[] { 1, "Stockholm", 65m });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Name", "Rate" },
+                values: new object[] { 2, "Uppsala", 55m });
+
+            migrationBuilder.InsertData(
+                table: "Services",
+                columns: new[] { "Id", "CityId", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, 1, "Fönsterputs", 300m },
+                    { 2, 1, "Balkongstädning", 150m },
+                    { 3, 2, "Fönsterputs", 300m },
+                    { 4, 2, "Balkongstädning", 150m },
+                    { 5, 2, "Bortforsling av skräp", 400m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
